3f03551dc2290f20268524977e09332cbaf06488
Sacha Morgese <sacha_morgese@waku-2.com>
2017-11-16 18:37:42 +0900

    First commit. Added modal file, still working on it.

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react'
 import '../css/main.css'
 
+const Helloer = () =>
+  (
+  <div>
+    <h1>Hello</h1>
+    <h2>Hello</h2>
+    <h3>Hello</h3>
+    <h4>Hello</h4>
+    <h5>Hello</h5>
+    <h6>Hello</h6>
+  </div>
+  )
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/main.js</code> and save to reload.
-        </p>
+        <Helloer className="left" />
+        <Helloer className="right" />
       </div>
     )
   }

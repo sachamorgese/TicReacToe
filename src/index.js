6a57f8bdfe0044416b2ea2aa5d482468df814133
Sacha Morgese <sacha_morgese@waku-2.com>
2018-04-03 14:48:33 +0900

    deactivated redux dev tools for production

--- src/index.js
+++ src/index.js
@@ -6,12 +6,18 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import ReduxThunk from 'redux-thunk'
 
-import App from './components/index'
-import reducers from './reducers/index'
+import App from './components'
+import reducers from './reducers'
 
 const createStoreWithMiddleware = applyMiddleware(ReduxThunk)(createStore)
+/* eslint-disable no-underscore-dangle */
+const reduxTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+/* eslint-enable */
 
-export const store = createStoreWithMiddleware(reducers)
+export const store = createStoreWithMiddleware(
+  reducers,
+  process.env.NODE_ENV === 'development' && reduxTools,
+)
 
 const root = document.getElementById('root')
 
